e6a764b893b5bf50c8670ff0efa9fb05a1cdd952
8ballhkm <580768hakim@gmail.com>
2025-06-14 04:14:40 +0800

    logout

--- users/urls.py
+++ users/urls.py
@@ -10,7 +10,6 @@ app_name = 'users'
 urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
