6503258f3504e674b0ecd29740702b0038690b24
8ballhkm <580768hakim@gmail.com>
2025-06-22 03:34:31 +0800

    cukk

--- listings/views.py
+++ listings/views.py
@@ -45,7 +45,7 @@ def home(request):
 def home2(request):
     latest_for_sale = Listing.objects.filter(listing_type='sale').order_by('-updated_at')[:5]
     latest_for_rent = Listing.objects.filter(listing_type='rent').order_by('-updated_at')[:5]
-    latest_listings = Listing.objects.order_by('-created_at')[:10]  
+    latest_listings = Listing.objects.order_by('-created_at')[:5]  
     
     context = {
         'latest_for_sale': latest_for_sale,
